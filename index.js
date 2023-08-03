e559e7e16ab5cb7f9cee0076d1e8571ca4522be4
Daví Lucas <127633524+Davilucasanteves@users.noreply.github.com>
2023-08-03 13:13:54 +0000

    2

--- index.js
+++ index.js
@@ -12,3 +12,21 @@ app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
 app.listen(port, () => {            // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
 });
+
+//index.js
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+import { selectUsuarios } from "./bd.js";
+
+app.get("/usuarios", async (req, res) => {
+  console.log("Rota GET/usuarios solicitada");
+  try {
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
