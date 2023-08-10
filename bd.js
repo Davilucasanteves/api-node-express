0b752ec4656b0b033671cf5f1214f4076c1eb8ce
Daví Lucas <127633524+Davilucasanteves@users.noreply.github.com>
2023-08-10 13:47:45 +0000

    1

--- bd.js
+++ bd.js
@@ -13,5 +13,23 @@ async function connect() {
     const res = await client.query("SELECT * FROM usuario");
     return res.rows;
   }
-  export { selectUsuarios };
+
+  //bd.js
+async function selectUsuario(id) {
+  const client = await connect();
+  const query = "SELECT * FROM usuario WHERE id = $1";
+  const usuario = [id];
+  const res = await client.query(query, usuario);
+  return res.rows;
+}
+
+//bd.js
+async function insertUsuario(data) {
+  const client = await connect();
+  const query = "INSERT INTO usuario (nome,senha,email) VALUES ($1,$2,$3) ";
+  const usuario = [data.nome, data.senha, data.email];
+  await client.query(query, usuario);
+}
+
+  export { selectUsuarios, selectUsuario, insertUsuario };
  
--- index.js
+++ index.js
@@ -19,13 +19,39 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-import { selectUsuarios } from "./bd.js";
+import { selectUsuarios, selectUsuario, insertUsuario } from "./bd.js";
+
+//index.js
+app.use(express.json());
 
 app.get("/usuarios", async (req, res) => {
   console.log("Rota GET/usuarios solicitada");
   try {
     const usuarios = await selectUsuarios();
     res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
+//index.js
+app.get("/usuario/:id", async (req, res) => {
+  console.log("Rota GET /usuario solicitada");
+  try {
+    const usuario = await selectUsuario(req.params.id);
+    if (usuario.length > 0) res.json(usuario);
+    else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
+//index.js
+app.post("/usuario", async (req, res) => {
+  console.log("Rota POST /usuario solicitada");
+  try {
+    await insertUsuario(req.body);
+    res.status(201).json({ message: "Usuário inserido com sucesso!" });
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
