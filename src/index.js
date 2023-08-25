6173c2c16fb53c539dc991336ec88ae466b24873
Daví Lucas <127633524+Davilucasanteves@users.noreply.github.com>
2023-08-25 11:38:49 +0000

    4

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
 import express from "express";      // Requisição do pacote do express
+import dotenv from "dotenv";
 import roteadorUsuario from "./routes/usuario.js"; //importa router
+dotenv.config();
+
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
-app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
+app.get("/", (req, res) => {
   res.json({
-    nome: "Daví Lucas",      // Substitua pelo seu nome
+    message: "API para CRUD usuario: link_gitgub",
   });
-  console.log("Rota / solicitada");
 });
 
 app.listen(port, () => {            // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
 });
 
-//index.js
-import dotenv from "dotenv";
 
-dotenv.config();
-
-
-//index.js
 app.use(express.json());
 app.use(roteadorUsuario);  //routerUsuario
 
